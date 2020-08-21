95d2b9a253acc5ec4c0233a57b1bb59735f19ed9
Srirag <vuppala@calpoly.edu>
2020-08-20 17:14:07 -0700

    clean code

--- src/AbstractMove.java
+++ src/AbstractMove.java
@@ -11,42 +11,6 @@ public abstract class AbstractMove extends AnimateEntity {
     }
 //
     protected abstract Point nextPosition( WorldModel world, Point destPos);
-//    {
-//        int horiz = Integer.signum(destPos.x - this.getPosition().x);
-//        Point newPos = new Point(this.getPosition().x+ horiz,
-//                this.getPosition().y);
-//
-//        Optional<Entity> occupant = world.getOccupant( newPos);
-//
-//        if (horiz == 0 ||
-//                (occupant.isPresent() && !(occupant.get().getClass() == Ore.class )))
-//        {
-//            int vert = Integer.signum(destPos.y - this.getPosition().y);
-//            newPos = new Point(this.getPosition().x, this.getPosition().y + vert);
-//            occupant = world.getOccupant( newPos);
-//
-//            if ((vert == 0) ||
-//                    (occupant.isPresent() && !(occupant.get().getClass() == Ore.class)))
-//            {
-//                newPos = this.getPosition();
-//            }
-//        }
-//
-//        return newPos;
-//    }
-    //9
-//    public void scheduleActions(
-//            EventScheduler scheduler,
-//            WorldModel world,
-//            ImageStore imageStore)
-//    {
-//        scheduler.scheduleEvent(this,
-//                Factory.createActivityAction(this, world, imageStore),
-//                this.getActionPeriod());
-//        scheduler.scheduleEvent(this,
-//                Factory.createAnimationAction(this, 0),
-//                this.getAnimationPeriod());
-//    }
 
     protected boolean move(WorldModel world, Entity target, EventScheduler scheduler){
         if (Point.adjacent(this.getPosition(), target.getPosition())){
